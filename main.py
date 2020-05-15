4a0006099014134f8984de01b8156cd3843d1314
Hweinstock <42325418+Hweinstock@users.noreply.github.com>
2020-05-14 17:19:10 -0700

    Fixed Commenting on tests

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ if __name__ == '__main__':
     """
     Testing
     """
-    dc = DataCollector(True)
-    dc.scrape_last_n_years_of_k_players(2, 10, "basic-info", "singles")
+    #dc = DataCollector(True)
+    #dc.scrape_last_n_years_of_k_players(2, 10, "basic-info", "singles")
     #dc.scrape_last_n_years_of_k_players(2, 10, "marital_status", "singles")
